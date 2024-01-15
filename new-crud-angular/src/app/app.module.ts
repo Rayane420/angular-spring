c3b66ddef2746403a843b61cd74743c63f542d02
Rayane Maciel Ribeiro <rayanemaciel10@gmail.com>
2024-01-15 20:11:54 -0300

    ajustando toolbar

--- new-crud-angular/src/app/app.module.ts
+++ new-crud-angular/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-//import {Component} from '@angular/core';
 
 @NgModule({
   declarations: [
@@ -26,16 +25,3 @@ import { MatButtonModule } from '@angular/material/button';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-/**
- * @title Toolbar overview
- */
-/*@Component({
-  selector: 'toolbar-overview-example',
-  templateUrl: 'toolbar-overview-example.html',
-  styleUrls: ['toolbar-overview-example.css'],
-  standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule]
-})
-export class ToolbarOverviewExample {}*/
-
